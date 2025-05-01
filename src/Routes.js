ee5fe350d2a3cf055c86d210ccf1633cd8465057
sylvia1213 <yeayoung1213@naver.com>
2025-05-01 16:07:59 +0900

    chore: 초기 세팅

---
+++ src/Routes.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Landing from './pages/Landing';
+import Login from './pages/Login';
+import Signin from './pages/Signin';
+import Main from './pages/Main';
+import Calling from './pages/Calling';
+import CallList from './pages/CallList';
+import Chatbot from './pages/Chatbot';
+
+class AppRouter extends React.Component {
+  render() {
+    return (
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/calling" element={<Calling />} />
+        <Route path="/callList" element={<CallList />} />
+        <Route path="/chatbot" element={<Chatbot />} />
+      </Routes>
+    );
+  }
+}
+
+export default AppRouter;
