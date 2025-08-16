f8cba8188360d580a0e5a3cc44602582e1b6fb60
hyom1n <hyomin0806@gmaile.com>
2025-08-16 23:54:09 +0900

    Twilio API 주소 변경

--- src/components/TwilioCallReceiver.js
+++ src/components/TwilioCallReceiver.js
@@ -26,8 +26,8 @@ const TwilioCallReceiver = () => {
     if (!jwtToken) return;
 
     const initTwilio = async () => {
-      // const res = await axios.get(`${API_BASE_URL}/api/token`, {
-      const res = await axios.get(`http://localhost:8080/api/token`, {
+      const res = await axios.get(`${API_BASE_URL}/api/token`, {
+        // const res = await axios.get(`http://localhost:8080/api/token`, {
         headers: { Authorization: `Bearer ${jwtToken}` },
       });
 
